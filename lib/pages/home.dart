f7a8bbd0546942e2c815b9fa29a8a66f1d24a64b
kiethippo <huynhkiet684@gmail.com>
2025-08-16 15:01:05 +0700

    add resource, fix bug svg

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -17,8 +19,27 @@ class HomePage extends StatelessWidget {
         ),
         backgroundColor: Colors.white,
         elevation: 0.0,
-        centerTitle: true,
+        centerTitle: true, // can giua cho tieu de
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          child: const Icon(Icons.arrow_back, color: Colors.black),
+          // child: SvgPicture.asset(
+          //   'assets\icon\Arrow - Left 2.svg',
+          //   cacheColorFilter: true,
+          // ),
+          decoration: BoxDecoration(
+            color: Color(0xff),
+            borderRadius: BorderRadius.circular(10),
+          ), // tao box cho nut back
+        ),
       ),
     );
+  }
+
+  Future<PictureInfo> _loadSvg(BuildContext context, String asset) async {
+    final svgString = await DefaultAssetBundle.of(context).loadString(asset);
+    final pictureInfo = await vg.loadPicture(SvgStringLoader(svgString), null);
+    return pictureInfo;
   }
 }
